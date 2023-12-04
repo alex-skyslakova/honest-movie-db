c4e622642db24c8152426f7b93de3c734c7422f0
Kristian Malak <xmalak@fi.muni.cz>
2023-12-04 18:51:23 +0100

    implemented error and loading /movies pages

---
+++ src/app/movies/error.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function error() {
+    return (
+        <div className="flex h-screen justify-center p-10">
+            Some error occurred, sorry
+        </div>
+    )
+}
