2ed27af9b68236953cf8c9be1b19a8a8f20f8c07
Kristian Malak <xmalak@fi.muni.cz>
2023-12-05 20:33:14 +0100

    switched elements in movie list item

--- src/components/MoviesListItem.tsx
+++ src/components/MoviesListItem.tsx
@@ -6,15 +6,15 @@ import Link from "next/link";
 export const MoviesListItem = ({movie}:{movie: Movie}) => {
     return (
         <Link href={`/movies/${movie.id}`} className="flex border-b gap-x-6 border-neutral-500 last:border-0">
-            <div className="flex flex-col text-center">
-                <Rating percentage={movie.rating}/>
-                <div>{movie.reviews.length} reviews</div>
-            </div>
+            <Image src={movie.image} alt="Image" width={0} height={0} sizes="100vw" className="w-auto h-32"/>
             <div className="flex flex-col gap-y-2">
                 <h4 className="text-xl">{movie.title}</h4>
                 <p>{movie.description}</p>
             </div>
-            <Image src={movie.image} alt="Image" width={0} height={0} sizes="100vw" className="ml-auto w-auto h-32"/>
+            <div className="flex flex-col text-center ml-auto">
+                <Rating percentage={movie.rating}/>
+                <div>{movie.reviews.length} reviews</div>
+            </div>
         </Link>
     )
 }
